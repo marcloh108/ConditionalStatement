63b48c49befb0615660d64263c3aae371fb10be7
marcloh108 <marc.loh@molisys.com>
2019-11-14 14:34:28 +0700

    Write a C program to calculate the root of a Quadratic Equation
    Create function to define and print
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- question4/main.c
+++ question4/main.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
+#include <math.h>
 
-int main()
+void main()
 {
-    int year;
-    printf("Input a year: ");
-    scanf("%d", &year);
-    if((year % 400) == 0)
-        printf("\nIt is a leap year");
-    else if ((year % 100) == 0)
-        printf("\nIt is not a leap year");
-    else if ((year % 4) == 0)
-        printf("\nIt is a leap year");
+   int a,b,c,d;
+   float x1,x2;
+
+   printf("Input the value of a,b & c : ");
+   scanf("%d%d%d",&a,&b,&c);
+   d=b*b-4*a*c;
+   if(d==0)
+   {
+     printf("Both roots are equal.\n");
+     x1=-b/(2.0*a);
+     x2=x1;
+     printf("First  Root Root1= %f\n",x1);
+     printf("Second Root Root2= %f\n",x2);
+   }
+   else if(d>0)
+    {
+       printf("Both roots are real and diff-2\n");
+       x1=(-b+sqrt(d))/(2*a);
+       x2=(-b-sqrt(d))/(2*a);
+       printf("First  Root Root1= %f\n",x1);
+       printf("Second Root root2= %f\n",x2);
+    }
     else
-        printf("\nIt is not a leap year");
-
-
-    return 0;
+        printf("Root are imaginary;\nNo Solution. \n");
 }
